2cea7f6ea197422bcebcde9b9badd6b2bb289623
michellekim622 <62683110+michellekim622@users.noreply.github.com>
2020-04-11 17:41:12 -0400

    Create areaofafield.py

---
+++ areaofafield.py
@@ -0,0 +1,15 @@
+import math
+
+length = input("provide the length of your property in feet: ")
+width = input("provide the width of your property in feet: ")
+
+length = int(length)
+width = int(width)
+
+area = length * width
+
+your_acres = area / 43560
+your_acres = str(your_acres)
+
+# # concatenation example
+print("Your area is: " + your_acres + " acres squared")
